0ae10ecede321a6e3e2e54e4348082c702d19c6a
Teja G <gowthumanikantasaranteja@gmail.com>
2026-09-08 14:29:05 +0530

    ✅ Palindrome Number | Easy | Python | Runtime: 13 ms (42.95%) | Memory: 12.5 MB | 2026-09-08

---
+++ leetcode/easy/0009-palindrome-number/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        
+        sol= str(x)
+        return sol == sol[::-1]
