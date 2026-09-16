1635b198b6cdfaf7eb8fb59269e645c77f2630e2
Teja G <gowthumanikantasaranteja@gmail.com>
2026-09-16 21:14:14 +0530

    ✅ BUSSEAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.py
+++ codechef/medium/BUSSEAT/solution.py
@@ -1,10 +1,1 @@
-# Bus Seating Problem
-
-T = int(input())  # number of test cases
-
-for _ in range(T):
-    N, K = map(int, input().split())
-    if K <= N:
-        print(0)
-    else:
-        print(2 * (K - N))
+# cook your dish here
