8b49523e5857393ffbe45667510a5bce88a10752
Teja G <gowthumanikantasaranteja@gmail.com>
2026-09-16 21:14:25 +0530

    ✅ SHARING | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/SHARING/solution.py
+++ codechef/medium/SHARING/solution.py
@@ -1,1 +1,14 @@
 # cook your dish here
+# Sharing Cookies Problem
+
+# Read input values
+A, B = map(int, input().split())
+
+# Check if total cookies can be divided equally
+if (A + B) % 2 != 0:
+    print(-1)
+else:
+    # Each should get half of total
+    target = (A + B) // 2
+    # Alice needs to give this many cookies to Bob
+    print(A - target)
